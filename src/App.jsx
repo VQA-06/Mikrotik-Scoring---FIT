67faba0e93e4d7a29de653e163139e48f716e8d9
vqa <vqa@cachyos>
2026-04-25 10:32:39 +0700

    fix metadata

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,8 @@ function App() {
   const [step, setStep] = useState(1);
   const [peserta, setPeserta] = useState([]);
   const [type, setType] = useState('mikrotik');
-  const url = "https://mscore.pages.dev";
-  const pImage = `${url}/preview.png`;
-
   return (
     <div className="relative min-h-dvh bg-base flex flex-col">
-      <title>FIT Scoring System</title>
-      <link rel="icon" type="image/svg+xml" href="/icon.png" />
-      <meta
-        name="description"
-        content="FIT Competition Scoring System"
-      />
-
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content={`${url}`} />
-      <meta property="og:title" content="Scoring System | FIT" />
-      <meta
-        property="og:description"
-        content="FIT Competition Scoring System"
-      />
-
-      <meta property="og:image" content={pImage} />
       <Header />
       <Main
         setPeserta={setPeserta}
